04945e0418f88891fb782cc24d01c90c68bf87b7
ben@fenker.org <ben@fenker.org@1a14d52f-a2f5-5141-256a-2a4f30bc3f75>
2012-09-11 18:33:07 +0000

    Added analysis manger to handle root histograms well.  See runAction

--- trunk/include/JTW_Event.hh
+++ trunk/include/JTW_Event.hh
@@ -1,8 +1,8 @@
 #include "K37EventGenerator.hh"
 
-class JTW_Event: public K37EventGenerator 
+class JTW_Event: public K37EventGenerator
 {
-	public:
-		/*int area (){ return (width * height / 2); };*/
-		void MakeEvent();
+public:
+  /*int area (){ return (width * height / 2); };*/
+  void MakeEvent();
 };
