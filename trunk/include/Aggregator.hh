c4df0763748292b8d0f3936c12c6b66c233407a2
gnilheb <gnilheb@1a14d52f-a2f5-5141-256a-2a4f30bc3f75>
2013-06-14 18:36:40 +0000

    I added in the Aggregator class from the Analyzer

---
+++ trunk/include/Aggregator.hh
@@ -0,0 +1,30 @@
+#ifndef Aggregator_h
+#define Aggregator_h 1
+
+#include <string>
+#include "K37_Data.hh"
+
+namespace AGG
+{
+   class Aggregator
+   {
+      public:
+         Aggregator();
+         ~Aggregator();
+         void RegisterData(K37_ABC::K37_Data *dataPointer_);
+         void RegisterIOMethod(const std::string &fileName_);
+         void BeginRun();
+         void EndEvent();
+         void EndRun();
+
+      private:
+         // to make Aggregator non-copyable
+         Aggregator(const Aggregator &); 
+         // to make Aggregator non-copyable
+         const Aggregator & operator=(const Aggregator &); 
+
+         class ImplementAGG;
+         ImplementAGG *mAGG; //stands for my Aggregator 
+   };
+}
+#endif
