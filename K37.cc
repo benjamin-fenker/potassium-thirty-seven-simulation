aecb479bab30adc53b550026982731cb45563ebe
ben@fenker.org <ben@fenker.org@1a14d52f-a2f5-5141-256a-2a4f30bc3f75>
2013-08-13 17:59:56 +0000

    Added cool perl script to make multi-threading interface nice.  Small updates to main program to accomodate perl script

--- K37.cc
+++ K37.cc
@@ -146,6 +146,7 @@ int main(int argc, char** argv) {
   event_action -> SetActiveChannels(&active_channels);
   event_action -> SetPrimaryGenerator(gen_action);
   runManager   -> SetUserAction(event_action);
+  the_aggregator -> SetEventAction(event_action);
 
   K37TrackingAction* tracking_action;
   runManager->SetUserAction(tracking_action= new K37TrackingAction());
@@ -180,8 +181,8 @@ int main(int argc, char** argv) {
 
   if (argc != 1) {  // batch mode
     G4String command = "/control/execute ";
-    G4String fileName = argv[1];
-    UImanager->ApplyCommand(command+fileName);
+    G4String fileName = argv[1]; 
+   UImanager->ApplyCommand(command+fileName);
   } else {       // interactive mode : define UI session
 #ifdef G4UI_USE
     G4UIExecutive * ui = new G4UIExecutive(argc, argv);
