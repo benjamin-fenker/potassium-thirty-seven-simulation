3b6f5110bdf2769f63766bc352405fcb5eab60c7
gnilheb@gmail.com <gnilheb@gmail.com@1a14d52f-a2f5-5141-256a-2a4f30bc3f75>
2013-10-14 21:12:21 +0000

    I have slowed down the memory leak.

--- trunk/include/K37RecoilMCPHit.hh
+++ trunk/include/K37RecoilMCPHit.hh
@@ -5,32 +5,60 @@
 
 #include "G4VHit.hh"
 #include "G4THitsCollection.hh"
+#include "G4Allocator.hh"
 
-class K37RecoilMCPHit : public G4VHit {
- private:
-  G4double energy_;
-  G4double time_;
-  G4double x_pos_;
-  G4double z_pos_;
-  G4int particle_pdg_;
-  G4int particle_charge_;
+class K37RecoilMCPHit : public G4VHit
+{
  public:
   K37RecoilMCPHit();
   ~K37RecoilMCPHit();
-  void SetEnergy(G4double energy) {energy_ = energy;}
-  void SetTime(G4double time) {time_ = time;}
-  void SetXPos(G4double x_pos) {x_pos_ = x_pos;}
-  void SetZPos(G4double z_pos) {z_pos_ = z_pos;}
-  void SetParticlePDG(G4int pdg) {particle_pdg_ = pdg;}
-  void SetParticleCharge(G4int charge) {particle_charge_ = charge;}
-  G4double GetEnergy() {return energy_;}
-  G4double GetTime() {return time_;}
-  G4double GetXPos() {return x_pos_;}
-  G4double GetZPos() {return z_pos_;}
-  G4int GetParticlePDG() {return particle_pdg_;}
-  G4int GetParticleCharge() {return particle_charge_;}
+  K37RecoilMCPHit(const K37RecoilMCPHit &right);
+  const K37RecoilMCPHit& operator=(const K37RecoilMCPHit &right);
+  G4int operator==(const K37RecoilMCPHit &right) const;
+
+  inline void *operator new(size_t);
+  inline void operator delete(void *aHit);
+
+  inline void SetEnergy(G4double energy_) {energy = energy_;}
+  inline G4double GetEnergy() {return energy;}
+  // ----------------------------------------------
+  inline void SetTime(G4double time_) {time = time_;}
+  inline G4double GetTime() {return time;}
+  // ----------------------------------------------
+  inline void SetXPos(G4double x_pos_) {x_pos = x_pos_;}
+  inline G4double GetXPos() {return x_pos;}
+  // ----------------------------------------------
+  inline void SetZPos(G4double z_pos_) {z_pos = z_pos_;}
+  inline G4double GetZPos() {return z_pos;}
+  // ----------------------------------------------
+  inline void SetParticlePDG(G4int pdg_) {particle_pdg = pdg_;}
+  inline G4int GetParticlePDG() {return particle_pdg;}
+  // ----------------------------------------------
+  inline void SetParticleCharge(G4int charge_) {particle_charge = charge_;}
+  inline G4int GetParticleCharge() {return particle_charge;}
+
+ private:
+  G4double energy;
+  G4double time;
+  G4double x_pos;
+  G4double z_pos;
+  G4int particle_pdg;
+  G4int particle_charge;
 };
 
 typedef G4THitsCollection<K37RecoilMCPHit> K37RecoilMCPHitsCollection;
+
+extern G4Allocator<K37RecoilMCPHit> K37RecoilMCPHitAllocator;
+
+inline void* K37RecoilMCPHit::operator new(size_t) {
+  void *aHit;
+  aHit = static_cast<void *>(K37RecoilMCPHitAllocator.MallocSingle());
+  return aHit;
+}
+
+inline void K37RecoilMCPHit::operator delete(void *aHit) {
+  K37RecoilMCPHitAllocator.
+      FreeSingle(static_cast<K37RecoilMCPHit*>(aHit));
+}
 
 #endif
