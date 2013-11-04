40f5ca0fd12c90bfe88fb755f9bc1435395d2f57
gnilheb@gmail.com <gnilheb@gmail.com@1a14d52f-a2f5-5141-256a-2a4f30bc3f75>
2013-11-04 18:14:02 +0000

    I have fixed build.sh so that the default behavior is to pop up a window to tell you that things are working.

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ ReleaseType="RELEASE" #you can choose to use the release build options
 cmake CMAKE_BINARY_DIR=$K37BUILDDIRECTORY -DGeant4_DIR=$PLACEOFGeant4ConfigCmake $K37SOURCEDIRECTORY -DCMAKE_CXX_FLAGS_DEBUG:STRING="$D_CPP_FLAGS" -DCMAKE_CXX_FLAGS_RELEASE:STRING="$O_CPP_FLAGS" -DCMAKE_BUILD_TYPE:STRING="$ReleaseType";
 make;
 if [ $? -eq 0 ] ; then
-#   ./K37;
+   ./K37;
 else
    printf '%s\n' "Make failed !!!";
 fi
