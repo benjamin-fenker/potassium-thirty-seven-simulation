adf973afb5d7cc43e2dc8339c6807b4fb68d9499
ben@fenker.org <ben@fenker.org@1a14d52f-a2f5-5141-256a-2a4f30bc3f75>
2014-02-27 20:35:19 +0000

    Resolution and messengers for all detectors added.

--- branches/DetectorResolution/include/K37Config.hh
+++ branches/DetectorResolution/include/K37Config.hh
@@ -1,5 +1,9 @@
-// Authors: Spencer Behling and Benjamin Fenker 2013
-
 // the configured options and settings for Tutorial
-#define K37_VERSION 24
+#ifndef K37Config_hh
+#define K37Config_hh 1
+
+#define K37_VERSION 111
+#define CONFIGURATION_DIRECTORY "/home/bfenker/geant4_workdir/K37Source/Configuration"
+
+#endif
 
