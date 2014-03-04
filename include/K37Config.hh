f0e444a715157f30759a2714c7a9dfe0732e5f72
ben@fenker.org <ben@fenker.org@1a14d52f-a2f5-5141-256a-2a4f30bc3f75>
2014-03-04 18:59:30 +0000

    Allowed via command line to enable storing random number seed.  Default is to not save the seed.

--- include/K37Config.hh
+++
@@ -1,9 +0,0 @@
-// the configured options and settings for Tutorial
-#ifndef K37Config_hh
-#define K37Config_hh 1
-
-#define K37_VERSION 111
-#define CONFIGURATION_DIRECTORY "/home/bfenker/geant4_workdir/K37Source/Configuration"
-
-#endif
-
