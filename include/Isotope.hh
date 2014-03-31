f597889b1ef73107a2d2f299b0e8186014b85dd2
gnilheb@gmail.com <gnilheb@gmail.com@1a14d52f-a2f5-5141-256a-2a4f30bc3f75>
2014-03-31 19:16:40 +0000

    I forgot to add some files so the previous commit will not work.

---
+++ include/Isotope.hh
@@ -0,0 +1,39 @@
+#ifndef Isotope_h
+#define Isotope_h 1
+
+#include <string>
+
+namespace ISO
+{
+   class Isotope
+   {
+      public:
+         Isotope(const std::string &filename_);
+         ~Isotope();
+         //Get the beta assymetry as a function of kinetic energy of e+
+         double A_beta   (const double &T_);
+         //Get the neutrino assymetry as a function of kinetic energy of e+ 
+         double B_nu     (const double &T_);
+         //Get the beta neutrino correlation as a function of kinetic energy of e+ 
+         double a_beta_nu(const double &T_);
+         //Get the alignment as a function of kinetic energy of e+ 
+         double c_align  (const double &T_);
+         //Get zhi as a function of kinetic energy of e+ 
+         double zhi      (const double &T_);
+
+         //This function can retrieve the value of a const stored in the 
+         //isotope database. Really this should not be used in simulations
+         //but is useful for debugging.
+         double FindValue(const std::string &key_) const;
+
+         //To make Isotope non-copyable
+         Isotope(const Isotope &) = delete;
+         const Isotope & operator=(const Isotope &) = delete;
+
+         //stands for my Implement Isotope
+         class ImplementI;
+         //my Isotope
+         ImplementI *mI;
+   };
+}
+#endif
